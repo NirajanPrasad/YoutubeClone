994a92ab19eac4719a2fbb22333375ad32b59d60
[Nirajan Prasad] <[nirajanprasad108@gmail.com]>
2023-08-02 21:57:12 +0530

    Completed the header section, including search and styling

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import { AppContext } from "../Context/ContextApi";
+import { Header } from "./";
 
 const App = () => {
   return (
     <AppContext>
-      <div>App</div>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<p>Home</p>} />
+            <Route path="/searchResult/:searchQuery" element={<p>Search</p>} />
+            <Route path="/video/:id" element={<p>Video</p>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppContext>
   );
 };
---
+++ src/img/index.js
@@ -0,0 +1,4 @@
+import ytLogo from "./yt-logo.png";
+import ytLogoMobile from "./yt-logo.png";
+
+export { ytLogo, ytLogoMobile };
