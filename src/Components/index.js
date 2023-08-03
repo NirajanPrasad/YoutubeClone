850be1fa57a668a17365fdc217980ae59e4245c7
Nirajan Prasad <nirajanprasad108@gmail.com>
2023-08-03 15:29:10 +0530

    Implemented the feed section and displayed it on ui

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Feed from "./Feed";
 import LeftNav from "./LeftNav";
 import LeftNavMenuItem from "./LeftNavMenuItem";
+import VideoCard from "./VideoCard";
 
-export { Header, Feed, LeftNav, LeftNavMenuItem };
+export { Header, Feed, LeftNav, LeftNavMenuItem, VideoCard };
